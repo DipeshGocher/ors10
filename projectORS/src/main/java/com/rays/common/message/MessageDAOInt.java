8bd9273690f98fcb069e7f09313b47a690fb274a
Vampinside <dipeshgurjer000.com>
2024-06-25 13:39:54 +0530

    done

---
+++ projectORS/src/main/java/com/rays/common/message/MessageDAOInt.java
@@ -0,0 +1,11 @@
+package com.rays.common.message;
+
+import com.rays.common.BaseDAOInt;
+
+/**
+ * Role DAO interface.
+ * Dipesh Gocher 
+ */
+public interface MessageDAOInt extends BaseDAOInt<MessageDTO> {
+
+}
